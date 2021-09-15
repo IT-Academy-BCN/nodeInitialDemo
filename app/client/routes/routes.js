7cff16951bda499479042dabc7000c4f2fd98771
Arnau Calduch <65604168+ArnauCalduch@users.noreply.github.com>
2021-09-15 11:09:50 +0200

    Add files via upload

--- app/client/routes/routes.js
+++ app/client/routes/routes.js
@@ -1,1 +1,11 @@
+const router = require('express').Router();
+const path = require("path");
 
+router.get('/login', (req, res,next) => {
+  res.sendFile(path.resolve(__dirname , '../public/index.html'));
+});
+router.get('/xat', (req, res,next) => {
+  res.sendFile(path.resolve(__dirname , '../public/xat.html'));
+});
+
+module.exports = router;
