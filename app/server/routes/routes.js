913eaa10f35eff0c94109a5ca1f8925307d01918
Arnau Calduch <65604168+ArnauCalduch@users.noreply.github.com>
2021-09-15 11:14:00 +0200

    Add files via upload

--- app/server/routes/routes.js
+++ app/server/routes/routes.js
@@ -1,1 +1,6 @@
+const router = require('express').Router();
+const controllerUser = require('../controllers/userController');
 
+router.post('/login', controllerUser.createUser);
+
+module.exports = router;
