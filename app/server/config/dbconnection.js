97cbfc6b4c9986a43b08af3dd7ac02878a7b0a37
Arnau Calduch <65604168+ArnauCalduch@users.noreply.github.com>
2021-09-15 11:12:27 +0200

    Add files via upload

--- app/server/config/dbconnection.js
+++ app/server/config/dbconnection.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+mongoose
+    .connect('mongodb://localhost:27017/xatdb', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) =>
+        console.error('It could not connect to MongoDB:', error.message)
+    );
+const db = mongoose.createConnection();
+  
+module.exports = db
