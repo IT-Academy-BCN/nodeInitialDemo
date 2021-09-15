bd965ad87977c807bc0cd0e4d15aac89c5e0c0a6
Arnau Calduch <65604168+ArnauCalduch@users.noreply.github.com>
2021-09-15 11:14:40 +0200

    Add files via upload

--- app/server/models/user.js
+++ app/server/models/user.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ai = require('mongoose-auto-increment');
 
+const userSchema = Schema({
+  username: String,
+  socket: String,
+  roomname: String,
+},);
+
+ai.initialize(mongoose.connection);
+userSchema.plugin(ai.plugin, 'User');
+module.exports = mongoose.model('User', userSchema);
