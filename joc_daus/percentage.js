5960d7fdab8883031fced9b35546546525ef3de0
xcomi <xaviercomi@gmail.com>
2021-11-29 00:18:51 +0100

    server, connect/create db & table ready

--- joc_daus/percentage.js
+++
@@ -1,7 +0,0 @@
-
-function calculatePercentage(wins, throws) {
-    let percentage = (wins * 100 / throws).toFixed(2) ;
-    return JSON.parse(percentage);
-};
-
-module.exports = {"calculatePercentage": calculatePercentage};
