2fbc5e045574662c2e3fa8ab7883f7f6d2b58747
Mariano <mariano_farace@hotmail.com>
2021-12-01 20:30:32 +0100

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  apps : [{
+  apps: [{
     script: 'index.js',
-    watch: '.'
+    watch: '.',
   }, {
     script: './service-worker/',
-    watch: ['./service-worker']
+    watch: ['./service-worker'],
   }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
 };
