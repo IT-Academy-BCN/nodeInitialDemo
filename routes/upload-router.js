60ce0d893e0e0869f7b9ecd8ca67e6c6296aa50c
Mariano <mariano_farace@hotmail.com>
2021-12-04 19:39:07 +0100

    Delete a TODO task

--- routes/upload-router.js
+++ routes/upload-router.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const { uploadImage } = require('../handlers/all-handlers');
-const router = express.Router();
 
-// TODO ver que haces con el storage este
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/upload.html'));
