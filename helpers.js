4b90edf8913bc3f17076f7eb5c601b72324322e2
Mariano <mariano_farace@hotmail.com>
2021-12-04 19:33:29 +0100

    Remove console.log

--- helpers.js
+++ helpers.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 const imageFilter = function (req, file, cb) {
   // Accept images only
-  console.log('entra al image filter');
   if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
     req.fileValidationError = 'Only image files are allowed!';
     return cb(new Error('Only image files are allowed!'), false);
