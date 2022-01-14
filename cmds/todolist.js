c70fd370807d973b8c5d99ab20f7856406340392
EnPere <pereruba@gmail.com>
2022-01-14 18:33:45 +0100

    archivo listtask2 + create2 + todolist.js agregados para ir testeando

---
+++ cmds/todolist.js
@@ -0,0 +1,23 @@
+//const create2 = require('./create2.js')
+//var nuvevaTarea = create2.nuevaLista
+
+const listaTareas = [
+  {
+    "taskName": "comprar pan",
+    "taskStartDate": "01-01-2022",
+    "taskEndDate": "02-02-2022",
+    "taskDescription": "comprar pan bimbo",
+    "taskUser": "pere"
+  },
+  {
+    "taskName": "beber agua",
+    "taskStartDate": "01-01-2022",
+    "taskEndDate": "02-02-2022",
+    "taskDescription": "beber agua clara",
+    "taskUser": "pere2"
+  }
+];
+
+module.exports = {
+  listaTareas
+};
