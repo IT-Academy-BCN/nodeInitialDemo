470e9589ef15010d2619c69df67af7acefe7100e
molivaresr <m.olivares.r@gmail.com>
2022-01-17 11:09:16 +0100

    Leer Json Updated

--- cmds/listTasks.js
+++ cmds/listTasks.js
@@ -7,8 +7,23 @@
 // Mensaje de Éxito y/o Error 
 // En todos los pasos siempre tener un mensaje de Error si algo falla, indicando el proceso que hace
 // Pej: Si falla a leer el archivo, Error: File not found o si falla al escribir Error: File can not write.
+const inquirer = require('inquirer'); // Para iniicar  preguntas por consola
+const fs = require('fs'); // Necesario para leer Json
 
-const inquirer = require('inquirer');
+// Inicia lectura de Json <--
+let rawtodo = fs.readFileSync('./database/tasks.json'); 
+const db = JSON.parse(rawtodo);
+const dbcache = db.tasks 
+//console.table(db.tasks) // Opcional para comprobar
+//console.table(dbcache) //  Opcional para comprobar 
+//db es un "objeto" con objetos
+//db.tasks permite acceder al contenido del array dentro de db.
+//dbcache es el objeto que utilizaremos y manejaremos
+
+// Fin de lectura del Json
+// -->
+
+
 // Código que te permite crear la pregunta sobre el campo que quieres crear
 inquirer
   .prompt([
--- leerjson.js
+++ leerjson.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
-const { join } =  require('path');
-const inbox = join(__dirname,'database');
-const outbox = join(__dirname,'database');
 
 let rawtodo = fs.readFileSync('./database/tasks.json');
 const db = JSON.parse(rawtodo);
