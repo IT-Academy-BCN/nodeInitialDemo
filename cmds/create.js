7af5a5b257f58a0aeea42534a4603aea8114036b
Blanket25 <bianca_inga@hotmail.com>
2022-01-22 17:20:52 +0100

    delete function added

--- cmds/create.js
+++ cmds/create.js
@@ -1,41 +1,27 @@
-// Pasos estándar a hacer en todas las funciones
-// TODO Leer archivo.json, crear si no existe.
-// TODO Mensaje de Éxito y/o Error 
-// TODO En todos los pasos siempre tener un mensaje de Error si algo falla, indicando el proceso que hace
-// Pej: Si falla a leer el archivo, Error: File not found o si falla al escribir Error: File can not write.
+const inquirer = require("inquirer"); // inicar  preguntas por consola
+const fs = require("fs"); // leer Json
+const { create } = require("../src/questions"); // Importa las preguntas de creación
+let dbcache = [];
 
-const inquirer = require('inquirer'); // Para iniicar  preguntas por consola
-const fs = require('fs'); // Necesario para leer Json
-const { create } = require('../src/questions') // Importa las preguntas de creación
-let dbcache = []
-
-// Inicia lectura de Json <--
-fs.readFile('../database/tasks.json', (err, rawdata) => {
-    if (!err) {
-     dbcache = JSON.parse(rawdata)
-     //console.log(dbcache)
-    } 
-    else {
-      //console.log('No se ha podido leer el archivo')
-      let emptyFile = JSON.stringify([{}],null,2);
-      fs.writeFile('../database/tasks.json', emptyFile, err => {
-          if(err) throw err; // error checking 
-      });
-      //console.error(err)
-    }
-}) 
-// Fin de lectura del Json
-// -->
-
-// Código que te permite crear las pregunta sobre el campo que quieres crear/modificar
-inquirer
-  .prompt(create)
-  .then( answers => {
-    dbcache.push(answers)
-    let todos = JSON.stringify(dbcache, null, 2);
-    fs.writeFile('../database/tasks.json', todos, err => {
-      if(err) throw err; // error checking
+fs.readFile("../database/tasks.json", (err, rawdata) => {
+  if (!err) {
+    dbcache = JSON.parse(rawdata);
+  } else {
+    let emptyFile = JSON.stringify([{}], null, 2);
+    fs.writeFile("../database/tasks.json", emptyFile, (err) => {
+      if (err) throw err;
     });
-    console.log('Tarea Creada');
-    console.table(dbcache);
+  }
+});
+
+inquirer.prompt(create).then((newTodo) => {
+  dbcache.push(newTodo);
+  let todos = JSON.stringify(dbcache, null, 2);
+
+  fs.writeFile("../database/tasks.json", todos, (err) => {
+    if (err) throw err;
   });
+
+  console.log("Tarea Creada");
+  console.table(dbcache);
+});
--- src/questions.js
+++ src/questions.js
@@ -1,97 +1,112 @@
 const create = [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Ingresa el nombre de la tarea:'
-    },
-    {
-      type: 'input',
-      name: 'startDate',
-      message: 'Fecha de Inicio:'
-    },
-    {
-      type: 'input',
-      name: 'endDate',
-      message: 'Fecha de Fin:'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Descripción de la tarea:'
-    },
-    {
-      type: 'input',
-      name: 'user',
-      message: 'Usuario Asignado:'
-    }
-  ];
+  {
+    type: "input",
+    name: "name",
+    message: "Ingresa el nombre de la tarea:",
+  },
+  {
+    type: "input",
+    name: "startDate",
+    message: "Fecha de Inicio:",
+  },
+  {
+    type: "input",
+    name: "endDate",
+    message: "Fecha de Fin:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Descripción de la tarea:",
+  },
+  {
+    type: "input",
+    name: "user",
+    message: "Usuario Asignado:",
+  },
+];
 
 const remove = [
   {
-    type: 'list',
-    name: 'dataBase',
-    message: 'Donde quieres buscar?:',
-    choices:['json','sql','mongo']
+    type: "list",
+    name: "dataBase",
+    message: "Donde quieres buscar?:",
+    choices: ["json", "sql", "mongo"],
   },
   {
-    type: 'input',
-    name: 'taskIndex',
-    message: 'Ingresa el indice de la que tarea vas a eliminar?:' //Decidir como identificar la tarea a eliminar 
+    type: "input",
+    name: "taskIndex",
+    message: "Ingresa el indice de la tarea que quieres eliminar?:",
   },
   {
-    type: 'list',
-    name: 'confirmation',
-    message: 'Deseas borrar?',
-    choices:['Yes','No']
+    type: "list",
+    name: "confirmation",
+    message: "Deseas borrar?",
+    choices: ["Yes", "No"],
   },
 ];
 
-const findOne = [ // Pendiente de definir
-   {
-      type: 'list',
-      name: 'dataBase',
-      message: 'Donde quieres buscar?:',
-      choices:['json','sql','mongo']
-    },
-    {
-      type: 'list',
-      name: 'confirmation',
-      message: 'Quieres buscar todas?',
-      choices:['Yes','No']
-    }
+const findOne = [
+  // Pendiente de definir
+  {
+    type: "list",
+    name: "dataBase",
+    message: "Donde quieres buscar?:",
+    choices: ["json", "sql", "mongo"],
+  },
+  {
+    type: "input",
+    name: "taskIndex",
+    message: "Ingresa el indice de la tarea que quieres ver:",
+  },
 ];
 
-const update = [ // Revisado Pere OK.
-    {
-      type: 'input',
-      name: 'taskindex',
-      message: 'Ingresa el indice de la tarea que quieres modificar?'
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Ingresa el NUEVO nombre de la tarea:'
-    },
-    {
-      type: 'input',
-      name: 'startDate',
-      message: 'NUEVA Fecha de Inicio:'
-    },
-    {
-      type: 'input',
-      name: 'endDate',
-      message: 'NUEVA Fecha de Fin:'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'NUEVA Descripción de la tarea:'
-    },
-    {
-      type: 'input',
-      name: 'user',
-      message: 'NUEVO Usuario Asignado:'
-    }
+const listAll = [
+  {
+    type: "list",
+    name: "dataBase",
+    message: "Donde quieres buscar?:",
+    choices: ["json", "sql", "mongo"],
+  },
 ];
 
-module.exports = {create, remove, findOne, update}  
+const update = [
+  {
+    type: "list",
+    name: "dataBase",
+    message: "Donde quieres buscar?:",
+    choices: ["json", "sql", "mongo"],
+  },
+  {
+    type: "input",
+    name: "taskindex",
+    message: "Ingresa el indice de la tarea que quieres modificar?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Ingresa el NUEVO nombre de la tarea:",
+  },
+  {
+    type: "input",
+    name: "startDate",
+    message: "NUEVA Fecha de Inicio:",
+  },
+  {
+    type: "input",
+    name: "endDate",
+    message: "NUEVA Fecha de Fin:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "NUEVA Descripción de la tarea:",
+  },
+  {
+    type: "input",
+    name: "user",
+    message: "NUEVO Usuario Asignado:",
+  },
+];
+
+module.exports = { create, remove, findOne, listAll, update };
