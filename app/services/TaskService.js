9b9a49c35b70d6765039216b3adf259496ca7d48
David Morales <dmorales@odec.es>
2022-01-30 11:35:01 +0100

    Starting task service

---
+++ app/services/TaskService.js
@@ -0,0 +1,23 @@
+
+function getTask(taskId) {
+
+}
+
+function getTasks() {
+
+}
+
+function createTask(task) {
+
+}
+
+function updateTask(task) {
+
+}
+
+function deleteTask(taskId) {
+
+}
+
+module.exports = {  getTask, getTasks, createTask, updateTask, deleteTask };
+
