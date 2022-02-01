5b2c60dbe44d3fc20dcf3e72df37cba5fe946755
dmoralesl <56760936+dmoralesl@users.noreply.github.com>
2022-02-01 16:17:55 +0100

    Adding comment to Task model

--- app/models/TaskModel.js
+++ app/models/TaskModel.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 class Task {
 
     // Initialize task and checking for some fields to be filled if value not provided
-    constructor(id, title, desc, isCompleted, createdAt, completedAt){
+    constructor(id, title, desc, comment, isCompleted, createdAt, completedAt){
         if (!title) {
             throw new Error('Title is required');
         }
@@ -11,6 +11,7 @@ class Task {
         this.id = id ? id : crypto.randomUUID();
         this.title = title;
         this.desc = desc;
+        this.comment = comment;
         this.isCompleted = isCompleted ? isCompleted : false;
         this.createdAt = createdAt ? createdAt : new Date();
         this.completedAt = completedAt ? completedAt : null;
