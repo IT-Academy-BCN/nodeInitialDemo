2fb7be110425a581c724a369b3437d5e015313e7
Guillem Parrado <guillempado@gmail.com>
2022-02-01 20:34:25 +0100

    mongoose - DB connection

--- app/app.js
+++ app/app.js
@@ -1,2 +1,28 @@
 require('dotenv').config()
-console.log(require('../model/Task'));
+
+
+// DISCUSSIÓ: TOT AIXÒ JA NO CAL FENT SERVIR AWAITS ALS FILES D'IMPLEMENTACIÓ DEL MODEL
+
+switch (process.env.STORAGE_TYPE) {
+    case "JSON": startJSONApp(); break;
+    case "MYSQL": startMySQLApp(); break;
+    case "MONGODB": startMongoDBApp(); break;
+}
+
+function startJSONApp() {
+
+}
+
+function startMySQLApp() {
+
+}
+
+async function startMongoDBApp() {
+    const mongoose = require("mongoose");
+    await mongoose.connect(process.env.MONGO_URI);
+    // TODO: Run app: Cridar funció d'start de la UI aquí. De moment executo demo
+    await require("./model/Tasks").demo();
+}
+
+
+
