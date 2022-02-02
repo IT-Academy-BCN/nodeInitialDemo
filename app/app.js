cb9526bd8d1393287f4d57abc6c7ce0c280f4c0d
Guillem Parrado <guillempado@gmail.com>
2022-02-02 11:12:37 +0100

    mysql connection

--- app/app.js
+++ app/app.js
@@ -14,7 +14,18 @@ function startJSONApp() {
 }
 
 function startMySQLApp() {
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    });
 
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log('Connected to MySQL Server!');
+    });
 }
 
 async function startMongoDBApp() {
