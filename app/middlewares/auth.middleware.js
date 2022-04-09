5e1c42954f94b0f01669b1ec4f4bee55f3c78fbc
David Morales <dmorales474@gmail.com>
2022-04-09 12:36:09 +0200

    fix workflow of auth middleware

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -1,31 +1,32 @@
 const basicAuth = (req, res, next) => {
-    const auth = req.headers.authorization;
-    if (!auth) {
-        res.send(401, {
-            status: false,
-            message: 'No authorization header found'
-        });
-    }
+  const auth = req.headers.authorization
+  if (!auth) {
+    return res.status(401).send({
+      status: false,
+      message: 'No authorization header found'
+    })
+  }
 
-    const [user, password] = auth.split(':');
+  const [user, password] = auth?.split(':') ?? [null, null]
 
-    if (!user || !password) {
-    res.send(401, {
-            status: false,
-            message: 'Authorization header has not the correct format <username>:<password>'
-        });
-    }
+  if (!user || !password) {
+    return res.status(401).send({
+      status: false,
+      message:
+        'Authorization header has not the correct format <username>:<password>'
+    })
+  }
 
-    if (user !== 'admin' || password !== 'admin') {
-        res.send(401, {
-            status: false,
-            message: 'Invalid credentials'
-        });
-    }
+  if (user !== 'admin' || password !== 'admin') {
+    return res.status(401).send({
+      status: false,
+      message: 'Invalid credentials'
+    })
+  }
 
-    next();
+  next()
 }
 
 module.exports = {
-    basicAuth
-};
+  basicAuth
+}
