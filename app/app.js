5a10f266cf8684cc741343e1fc3197156d2bd3a7
Luis Quevedo <qfluis@gmail.com>
2022-04-22 10:03:19 +0200

    Nivel 3 finalizado. Pendiente refactorizar

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');  // para leer body petición POST
-const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload'); // otra opción multer: https://www.npmjs.com/package/multer 
 const cors = require('cors');
-// otra opción multer: https://www.npmjs.com/package/multer 
 const path = require('path');
 
 app.use(bodyParser.json());
@@ -15,9 +14,13 @@ app.use(fileUpload({
 
 const PORT = 5555;
 
+const respuesta = (req, res) => {
+    
+}
+
 // Nivel 1 Ejercicio 1 ##################################################################
 app.get('/user', (req, res) => {
-    res.json({
+    res.status(200).json({
         name: 'Luis',
         edad: 41,
         url: req.hostname + req.originalUrl
@@ -29,9 +32,9 @@ app.post('/upload', (req, res) => {
     
     const img = req.files?.imgfile;
     //const img = (req.files) ? req.files.img : null; 
-    
+   
     if (!img) { // !req.files || !req.files.imgfile
-        res.json({
+        res.status(400).json({
             status: "Error",
             msg: "No se ha subido fichero"
         });
@@ -43,13 +46,23 @@ app.post('/upload', (req, res) => {
     const extensionesValidas = ['png','jpg','gif'];
 
     if (!extensionesValidas.includes(imgExt)){
-        res.json({
+        res.status(415).json({  // Unsuported media type...
             status: "Error",
             msg: "Extensión " + imgExt + " no válida. Las extensiones válidas son: " + extensionesValidas
         });
         return;
     }
-    
+
+    /* // imagenes no validas NO FUNCIONA
+    if (img.mimetype.indexOf('image') === -1 ) {
+        res.status(415).json({  // Unsuported media type...
+            status: "Error",
+            msg: "Archivo dañado y/o no permitido"
+        });
+        return;
+    }
+    */
+
     const f = (new Date()).toISOString();
     const marcaFecha = f.replaceAll(':','-').replace('T','-').replace('.','-').replace('Z','');
     console.log(marcaFecha);
@@ -58,13 +71,13 @@ app.post('/upload', (req, res) => {
 
     img.mv(imgPath, ( err ) => {
         if ( err ){
-            res.json({
+            res.status(500).json({ // ¿error del server?
                 status: "Error",
                 err
             });
         }
 
-        res.json({
+        res.status(200).json({
             status:"OK",
             msg: "imagen subida"
         });
@@ -72,63 +85,58 @@ app.post('/upload', (req, res) => {
 });
 
 
-// Nivel 2 Ejercicio 1 ##################################################################
+// Nivel 2 Ejercicio 1 + Nivel 3 Ejercicio 1 ############################################
 /*
 TODO: DUDAS: no se si tienen que ser los dos middlewares en uno solo o así vale.
 No tengo claro si se aplican a /time o a todo.
 */
 
 app.use('/time', (req, res, next) => {
-    //res.set('Cache-control', 'public, max-age=300');
-    res.set('Cache-control', 'no-cache');
+    //res.set('Cache-control', 'public, max-age=0');
+    res.set('Cache-control', 'no-cache'); 
     next();
 });
 
-
 app.use('/time', cors()); 
 
-
+// N3 E1
+app.use('/time', (req, res, next) => {
+    const user = req.headers.user;
+    const pass = req.headers.pass;
+    if(!user || !pass) {
+        res.status(401).json({
+            status: "Error",
+            msg:"Usuario y/o contraseña no proporcionados"
+        });
+        return;
+    }
+    // damos por hecho que user y pass son correctos
+    next();
+});
 
 app.post('/time', ( req, resp ) => {
     const user = req.body?.username;
     if(!user) {
-        resp.json({
+        resp.status(401).json({
             status:"Error",
             msg: "Usuario no indicado"
         });
         return
     }
-
     const today =(new Date()).toISOString().split('T');
 
-    resp.json({
+    resp.status(200).json({
         fecha: today[0],
         hora: today[1].substring(0,8)
     })
 })
 
-
-
-// Nivel 3 Ejercicio 1 ##################################################################
-/*
-Nivell 3
-- Exercici 1
-Afegeixi un middleware a l'endpoint anterior que retorni un HTTP 
-Status 401 - Unauthorized si la capçalera de la petició no conté 
-autenticació bàsica (usuari i contrasenya).
-*/
-
-
-
-
-// TODO: Códigos de error
-
-//TODO: readme en condiciones
-
 // 404
 app.use((req, res, next) => {
     res.status(404).json({status: "Error", msg: 'Ups! not found'});
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
