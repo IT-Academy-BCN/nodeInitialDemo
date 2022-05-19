2bc5a20997cb0a24285b787d12c60013ad45f700
eliashz <eliashz@gmail.com>
2022-05-19 09:43:15 +0200

    starting menu

--- app/app.js
+++ app/app.js
@@ -7,3 +7,17 @@
 - deleteTask()
 - showTaskState() returns {(pending/ executing/ completed), init date, completation date, userId}
 
+  */
+
+const showMenu = () => {
+    console.clear();
+    console.log("----- TO-DO App -----");
+    console.log('1. Add Task');
+    console.log('2. Update Task');
+    console.log('3. Delete Task');
+    console.log('4. List Task');
+    console.log('5. List All');
+    console.log('6. Show Task State');
+}
+
+showMenu();
