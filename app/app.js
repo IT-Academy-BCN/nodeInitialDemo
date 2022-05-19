e2f4b218447aa5a549a4ea697afb7037f147405a
eliashz <eliashz@gmail.com>
2022-05-19 10:15:25 +0200

    promise

--- app/app.js
+++ app/app.js
@@ -10,33 +10,47 @@
   */
 
 const showMenu = () => {
-    console.clear();
-    console.log("----- TO-DO App -----");
-    console.log('1. Add Task');
-    console.log('2. Update Task');
-    console.log('3. Delete Task');
-    console.log('4. List Task');
-    console.log('5. List All');
-    console.log('6. Show Task State');
-    console.log('0. Exit \n');
 
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    readline.question('Select option: ', (opt) => {
-        readline.close();
+    return new Promise ((resolve, reject) => {
+        console.clear();
+        console.log("----- TO-DO App -----");
+        console.log('1. Add Task');
+        console.log('2. Update Task');
+        console.log('3. Delete Task');
+        console.log('4. List Task');
+        console.log('5. List All');
+        console.log('6. Show Task State');
+        console.log('0. Exit \n');
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('Select option: ', (opt) => {
+            readline.close();
+            resolve(opt);
+        });
     });
 }
 const pause = () => { 
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    readline.question('\n Press enter to continue\n', (opt) => {
-        readline.close();
+    return new Promise ((resolve, reject) => {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        readline.question('\n Press enter to continue\n', (opt) => {
+            readline.close();
+            resolve();
+        });
     });
 }
 
-showMenu();
-pause();
+const main = async() => {
+    let opt = '';
+    do {    
+        opt = await showMenu();
+    } while (opt !== '0');     
+    //pause();
+}
+main();
