84e771184ce4c464796885e101158db8824c4128
MK <caprichismo@gmail.com>
2022-05-19 12:43:05 +0200

    adding functions with right search logic

--- app/functions.js
+++ app/functions.js
@@ -9,81 +9,67 @@ let readData = () => {
   // La Array LLegida del JSON 
   let readArray = JSON.parse(fs.readFileSync(fileName, 'utf8'));
   return readArray;
- 
+  /*
+  if (parametre == "Json") {
+
+    
+  }
+  else if (parametre == "Mysql"){
+
+  }
+  */
 }
+
+
 // Write JSON.  Nombre Funcion = WriteData(). => Desde una array 
 let writeData = (enterArray) => {
+
+  // Write JSON
   fs.writeFile('./todo.json', JSON.stringify(enterArray), (error) => {
     if (error) {
         throw error;
     }
   });
+
 };
 
-
-
-
+// Create Search by Id => Des d'una Array i un objId
 const searchById = ((dataInput, idInput) => {
-  index = dataInput.findIndex(task => task.taskId === idInput);
-  return index;
+  return dataInput.findIndex(task => task.taskId === idInput);
 })
 
 
+//addTask() Function
+const addTask = (userNameInput,taskNameInput) => {
+  // read todo From Json 
+  let data = readData();
+  // Find New Task Id
+  let newTodoId = data[data.length - 1].taskId + 1;
+  // Create new todo task
+  let newTodo = new Todo (newTodoId, userNameInput, taskNameInput );
+  // Add New Task to Data Array
+  data.push(newTodo);
+  // Save data to Json
+  writeData(data);
+  // Console log
+  console.log(`New Task "${taskNameInput}" added by "${userNameInput}"`);
+}
+
+addTask("!enric", "fer el dropo")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+const todo1 = new Todo (7, "enric", "fer to do" );
+//{"idObj": 1,"userName":"someName 1", "descr":"some text 1", "state": "pending", "initDate": "2022-05-17T10:32:13.539Z", "complDate": null}
+console.log(todo1);
+*/
+/*
+- listTask()
+- listAll()
+- updateTask()
+- deleteTask()
+- showTaskState() returns {(pending/ executing/ completed), init date, completation date, userId}
+*/
 
 
 
@@ -157,7 +143,6 @@ const listTask = (idInput) => {
 
 
 
-
 //upDatetask, takes numTask id = position in array passed as argument
 const updateTask = (idInput, stateInput) => {
   let result = "";
@@ -165,13 +150,15 @@ const updateTask = (idInput, stateInput) => {
   let index = searchById(data,idInput);
   let currentTask = data[index];
   if(!currentTask) {return 'Task not found.'};
+  //assigning new state
    currentTask.state = stateInput;
+  //write data
    writeData(data);
    console.log(`The task with id ${currentTask.taskId} is ${currentTask.state}`);
   }
 
 
-//Virtually no difference between listTask and showTaskState????????
+//shows state of task
 const showTaskState = (idInput) => {
   let result = "";
   let data = readData();
@@ -190,17 +177,6 @@ const showTaskState = (idInput) => {
 
 //module.exports = {addTask, listTask, listAll, updateTask, deleteTask, showTaskState};
   
-
-
-
-
-
-
-
-
-
-
-
 
 
 
