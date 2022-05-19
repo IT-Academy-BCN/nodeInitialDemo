4644ac3b88db4d2b602909c12492d33b8a76ae17
eliashz <eliashz@gmail.com>
2022-05-19 13:20:37 +0200

    app fin, falta conectar

--- app/app.js
+++ app/app.js
@@ -1,22 +1,10 @@
-/* const functions {addTask, 
+const functions {addTask, 
                   listTask,
                   listAll
                   updateTask,
                   deleteTask,
                   showTaskState 
-                } = require('./functions') */
-
-/* addTask()
-- listTask()
-- listAll()
-- updateTask()
-- deleteTask()
-- showTaskState() returns {(pending/ executing/ completed), init date, completation date, userId}
-
-  */
-const addTask = (userame, description) => {
-    console.log(`Task ${description} created for ${userName}.`);
-}
+                } = require('./functions');
 
 const showMenu2 = (question) => {
     return new Promise ((resolve, reject) => {
@@ -75,25 +63,28 @@ const main = async() => {
         opt = await showMenu();
         switch (opt) {
             case '1':
-                const username = await showMenu2('Write your username: ');
+                const userName = await showMenu2('Write your username: ');
                 const description = await showMenu2("Write task's description: ");
-
+                addTask(userName, description);
             break;
-
             case '2':
-                updateTask();
+                const id2 = await showMenu2('Write ID to update: ');
+                updateTask(id2);
             break;
             case '3':
-                deleteTask();
+                const id3 = await showMenu2('Write ID to delete: ');
+                deleteTask(id3);
             break;
             case '4':
-                listTask();
+                const id4 = await showMenu2('Write ID to list: ');
+                listTask(id4);
             break;          
             case '5':
                 listAll();
             break;
             case '6':
-                showTaskState();
+                const id6 = await showMenu2('Write ID to show state: ');
+                showTaskState(id6);
             break;
         }
         if (opt !== '0') await pause();
