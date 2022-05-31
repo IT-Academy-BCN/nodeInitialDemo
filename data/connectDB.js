66d150fbe3c542210bb19b95b01bb52374f6698b
Patricia <waltraute@hotmail.com>
2022-05-31 20:49:12 +0200

    connectDB creat

---
+++ data/connectDB.js
@@ -0,0 +1,10 @@
+const config = require('../config/config');
+
+if(config.dialect === 'mongo'){
+    const {connectMongoDB} = require('./connectMongoDB');
+    connectMongoDB()
+    }
+if(config.dialect === 'mysql'){
+    const {connectMySQL} = require('./connectMySQL')
+    connectMySQL()
+    }
