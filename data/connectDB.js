7e177e094ba3a4b1663dc6e86558ed8cc1f3fb91
Patricia <waltraute@hotmail.com>
2022-05-31 21:15:35 +0200

    modificado connectDB

--- data/connectDB.js
+++ data/connectDB.js
@@ -1,9 +1,5 @@
 const config = require('../config/config');
 
-if(config.dialect === 'mongo'){
-    const {connectMongoDB} = require('./connectMongoDB');
-    connectMongoDB()
-    }
 if(config.dialect === 'mysql'){
     const {connectMySQL} = require('./connectMySQL')
     connectMySQL()
