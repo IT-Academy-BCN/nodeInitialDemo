6a500d3dbcd822de30f08893a102290fcf8a8c2e
Patricia <waltraute@hotmail.com>
2022-05-31 19:33:23 +0200

    creat class Player

---
+++ models/player.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Player {
+
+    constructor(name){
+        this.idPlayer = null
+        this.namePlayer = name
+        this.register_date = new Date
+    }
+       }
+module.exports = Player
