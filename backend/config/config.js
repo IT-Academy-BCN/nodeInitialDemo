db13446dede23677374e6bb63ac948f2ea901f25
Patricia <waltraute@hotmail.com>
2022-06-22 18:00:09 +0200

    modificar carpetes

---
+++ backend/config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    user: "root",
+    password: "root1111_",
+    database: "xatroom",
+    host: "localhost",
+    dialect: "mysql",
+    
+    port: process.env.PORT || 8888,
+  }
+  
+  
+  module.exports = config
