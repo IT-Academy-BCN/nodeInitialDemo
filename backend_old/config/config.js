74b1e408dfb2c27d5426bc70f46c266cac566fdb
Patricia <waltraute@hotmail.com>
2022-07-02 23:40:05 +0200

    esborrat old files

--- backend_old/config/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  user: "root",
-  password: "root1111_",
-  database: "xatroom",
-  host: "localhost",
-  dialect: "mysql"
-};
-module.exports = config;
--- client_old/config.js
+++
@@ -1,1 +0,0 @@
-module.exports = {port: process.env.PORT || 5001}
