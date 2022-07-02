fb0e74af5069569f28d4f48893778992079ffedc
Patricia <waltraute@hotmail.com>
2022-07-02 20:05:35 +0200

    server client ok

--- client/client.js
+++ client/client.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
 const config = require("./config");
-require ( 'dotenv' ).config();
 const app = express();
 const client = require("http").createServer(app);
-const io = require("socket.io")(client);
+//const io = require("socket.io")(client);
 
 app.use(express.static(path.join(__dirname + "/../client")));
 
