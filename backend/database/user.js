5f0d63dc0e64c4ef9cf7adfbe77d4f080fb82297
Patricia <waltraute@hotmail.com>
2022-07-05 00:08:44 +0200

    model-taula user

---
+++ backend/database/user.js
@@ -0,0 +1,39 @@
+const mysql = require("mysql2");
+const Sequelize = require("sequelize");
+const config_db = require("../config/config");
+const connectMySQL = require("./connect");
+const sequelize = new Sequelize(
+  config_db.database,
+  config_db.user,
+  config_db.password,
+  
+  { dialect: config_db.dialect,
+  logging: false ,
+define: {
+  freezeTableName: true  //el nombre de la tabla es igual al nombre del modelo
+},
+  },
+);
+
+sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+  });
+
+   module.exports = {
+    mysql,
+    connectMySQL,
+    sequelize,
+    user
+  };
+  
+
+  
