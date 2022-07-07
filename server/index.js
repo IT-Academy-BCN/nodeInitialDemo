3cf738dd0c71bf3b8c1bae318a0e895684d86e29
MK <caprichismo@gmail.com>
2022-07-07 15:24:52 +0200

    add adding order and prel routes in index.js

--- server/index.js
+++ server/index.js
@@ -1,42 +1,39 @@
 require('dotenv').config();
+
 const express = require('express');
-
-
+const cors = require('cors');
 
 const app = express();
-
-const connectDB = require('./utils/connectDB.js')
-
 //create server
-const http = require('http').Server(app);
-
-//create socket server
-const io = require('socket.io')(http);
+const server = require('http').Server(app);
 const port = process.env.PORT || 3000;
+//create socket server
+const io = require('socket.io')(server,{
+  cors: {
+    ortigin: '*'
+  }
+});
 
-//require('../utils/sockets')(io);
+//connect to DB
+require('./utils/connectDB.js')();
 
-connectDB();
 
 app.use(express.static( '../public'));
+app.use(cors);
 
+app.use('/register', registerRoutes);
+app.use('/login', loginRoutes);
+app.use('/auth', authRoutes);
 
-io.on('connection', (socket => {
-  
-  //console.log(socket.id);
-  socket.emit('welcome-message', {message: 'Welcome to the server', date: new Date()} )
-
-
-
-}))
 
 //invalid route handling
-
 app.use((req, res, next)=>{
     res.status(404).send({message:"Bad request: Route Not Found"});
   });
 
- 
-  http.listen(port, () => {
+//requiering and executing sockets
+require('../utils/sockets')(io);
+
+server.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
   });
