7e449bc05133f1e3b4ca4570ce16861530c5bccb
MK <caprichismo@gmail.com>
2022-07-07 10:21:06 +0200

    add mover useful info links from sockets.js to readme

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = async() => {
     
     try {
-       await mongoose.connect(process.env.MONGO_URL);
+       await mongoose.connect(process.env.DB_URL);
         console.log('DB connected successfully.');
     } catch(err){
         console.log('Error when connecting to DB.');
