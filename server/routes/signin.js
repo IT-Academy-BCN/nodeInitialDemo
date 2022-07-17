ccf487c6468b57ec1b4f84dd37be2dd28981e3e4
MK <caprichismo@gmail.com>
2022-07-17 17:55:05 +0200

    added controllers

--- server/routes/signin.js
+++ server/routes/signin.js
@@ -2,8 +2,9 @@ const express = require('express');
 const api = express.Router();
 
 const signin = require('../controllers/signin');
+//const auth = require('./middleware/authenticate.js');
 
-api.post('/signin',
+api.post('/signin', //auth,
              signin);
 
 module.exports = api;
