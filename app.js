4a67b7205e876be8429bea18008364823bcdade2
Victor <vlopezmoles@gmail.com>
2022-07-20 09:38:23 +0200

    update dev_teams

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const { mostrarMenu } = require('./helpers/mensajes.js');
+
+console.clear()
+
+const main = async() => {
+    console.log('Hola Mundo');
+    
+    
+    mostrarMenu();
+    
+    
+}
+    
+main()
