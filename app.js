1c8caff4194a9753c28757fbf573c2c78deb7366
Victor <vlopezmoles@gmail.com>
2022-07-28 09:40:01 +0200

    test01

--- app.js
+++ app.js
@@ -2,14 +2,3 @@
 const { mostrarMenu } = require('./helpers/mensajes.js');
 
 console.clear()
-
-const main = async() => {
-    console.log('Hola Mundo');
-    
-    
-    mostrarMenu();
-    
-    
-}
-    
-main()
