e87cef09ed0a3a8acfb68ed9244ce29bca2bca4f
Jess <jessicafala@gmail.com>
2022-09-15 12:57:09 +0200

    .

--- 4.1_REST API/src/controllers/time.js
+++ 4.1_REST API/src/controllers/time.js
@@ -8,3 +8,26 @@ const time = (req, res) => {
  };
 
  module.exports = {time}
+
+ class Authenticate extends Middleware
+ {
+
+/*
+  //AQUI ES DONDE GENERA EL MENSAJE DE NO AUTENTICADO
+  protected function unauthenticated($request, array $guards)
+  {
+      throw new AuthenticationException(
+          'Unauthenticated.', $guards, $this->redirectTo($request)
+      );
+  }
+
+  
+
+  //AQUI SE SUPONE ME RETORNA A LOGIN PERO NO LO HACE
+  protected function redirectTo($request)
+  {
+      if (! $request->expectsJson()) {
+          return route('login');
+      }
+  }
+} */
