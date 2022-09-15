05203ea1d4c4e1cbf4ef29b6bfde16a98504e625
Jess <jessicafala@gmail.com>
2022-09-15 12:26:37 +0200

    .

--- src/middlewares/cache.js
+++ src/middlewares/cache.js
@@ -2,7 +2,7 @@
 
 // Configuració del Middleware per fer el control del caché (mètodes "POST", CORS)
   
-let cacheMiddleware = (req,res,next)=>{
+let cacheMiddleware = (req,res,next)=> {
   if (req.method == 'POST') {
     res.set("Cache-control", "No-cache");
 } else {
@@ -12,3 +12,9 @@ next();
 }
     
 module.exports = cacheMiddleware;
+
+/*Nivel 2
+Afegeix un middleware a l'endpoint anterior 
+que retorni un HTTP Status 401 - Unauthorized 
+si la capçalera de la petició no conté autenticació bàsica 
+(usuari/ària i contrasenya). */
