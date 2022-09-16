639a30eaf7e76e06cbf6f8aa80e98ea6b01ca728
Jess <jessicafala@gmail.com>
2022-09-16 11:42:52 +0200

    ok

--- 4.1_REST API/app.js
+++ 4.1_REST API/app.js
@@ -3,7 +3,7 @@
 // Importar dotenv
 require('dotenv').config();
 
-// Importar llibreria Express
+// Importar llibreries 
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
@@ -32,6 +32,6 @@ app.use('/', user_routes);
 
 // Iniciar servidor--> ¡¡¡SEMPRE AL FINAL!!!
 app.listen(PORT, () => {
-    console.log(`El servidor funciona en el port ${PORT}. // El servidor funciona al port ${PORT}. // Server works on port ${PORT}.`);
+    console.log(`El servidor funciona en el puerto ${PORT}. // El servidor funciona al port ${PORT}. // Server works on port ${PORT}.`);
 });
 
