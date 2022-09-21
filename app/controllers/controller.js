b49dc077b7a8d9ac434df69d8b0b235ebb6ad8f3
deltakosmos <francescbages@gmail.com>
2022-09-21 14:08:57 +0200

    Rutes arreglades al controller del nivell 1

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -3,12 +3,12 @@ const fs = require('fs/promises');
 
 async function borrarFitxer(objecteBusca) {
     try {
-        let cargarFitxer = await fs.readFile('D:/Dades/Francesc/Github_repos/nodeInitialDemo-1/app/models/todo.json', { encoding: 'utf8' }); //cargar fitxer
+        let cargarFitxer = await fs.readFile('../models/todo.json', { encoding: 'utf8' }); //cargar fitxer
         let llistatTodo = JSON.parse(cargarFitxer); //converteix en objecte        
         let propietat = Object.keys(objecteBusca); //Identificar propietat de busca 
         let todoTrobat = llistatTodo.findIndex(todo => todo[propietat[0]] == objecteBusca[propietat[0]]); //Buscar el PRIMER objecte amb la propietat buscada
         llistatTodo.splice(todoTrobat,1); //Eliminar el primer objecte de l'array
-        await fs.writeFile('D:/Dades/Francesc/Github_repos/nodeInitialDemo-1/app/models/todo.json',JSON.stringify(llistatTodo)); //Sobreescriu tot el fitxer amb el nou array
+        await fs.writeFile('../models/todo.json',JSON.stringify(llistatTodo)); //Sobreescriu tot el fitxer amb el nou array
         return console.log("L'objecte ha sigut el.liminat de l'array");
     } catch (err) {
         console.error(err);
@@ -19,17 +19,18 @@ async function borrarFitxer(objecteBusca) {
 
 async function actualitzarFitxer(objecteBusca,nouValor) {
     try {
-        let cargarFitxer = await fs.readFile('D:/Dades/Francesc/Github_repos/nodeInitialDemo-1/app/models/todo.json', { encoding: 'utf8' }); //cargar fitxer
+        let cargarFitxer = await fs.readFile('../models/todo.json', { encoding: 'utf8' }); //cargar fitxer
         let llistatTodo = JSON.parse(cargarFitxer); //converteix en objecte        
         let propietat = Object.keys(objecteBusca); //Identificar propietat de busca 
         let todoTrobat = llistatTodo.findIndex(todo => todo[propietat[0]] == objecteBusca[propietat[0]]); //Buscar el PRIMER objecte amb la propietat buscada
         llistatTodo[todoTrobat][propietat] = nouValor;
-        await fs.writeFile('D:/Dades/Francesc/Github_repos/nodeInitialDemo-1/app/models/todo.json',JSON.stringify(llistatTodo)); //Sobreescriu tot el fitxer amb el nou array
+        await fs.writeFile('../models/todo.json',JSON.stringify(llistatTodo)); //Sobreescriu tot el fitxer amb el nou array
         return console.log("L'objecte ha sigut actualitzat de l'array");
     } catch (err) {
         console.log(err);
     }
 }
+
 
 //actualitzarFitxer({ "usuari": "Francesc" },"Joan");
 
@@ -54,6 +55,7 @@ async function llistarTotesLesTasques() {
     }
 }
 
+//llistarTotesLesTasques();
 
 async function llistarPerEstat(estatTasca) {
     try {
@@ -84,6 +86,7 @@ async function llistarPerEstat(estatTasca) {
     }
 }
 
+llistarPerEstat('finalitzada')
 
 module.exports = {
     borrarFitxer,
