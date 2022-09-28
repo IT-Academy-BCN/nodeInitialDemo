2623e211b17da71653cee9bd56b3c0f58afd39c4
unknown <francescbages@gmail.com>
2022-09-28 10:56:01 +0200

    Inquirer usuari i menu principal

--- app/app.js
+++ app/app.js
@@ -1,29 +1,58 @@
 const inquirer = require('inquirer');
-const controlLocal = require('./controllers/controller.js');
+const controlLocal = require('./controllers/controller');
+const controllerInquirer = require('./controllers/controllerInquirer');
 
-/// MAIN
+/// Variable usuari
+let usuari;
+
+/// Sequencia
 console.log('DevTeams Sprint 3.3');
-console.log('Comença el formulari');
-ordrefuncions();
 
-// ordrefuncions();
-function ordrefuncions(){
-  //console.clear();
-  let resposta1 = preguntaInicial();
-  return (resposta1)
+(async function principal(){
+
+//Menu usuari
+console.log('Menú usuari');
+await preguntaUsuari();
+
+///Menu principal
+console.log('Menú principal');
+await preguntaMenu(usuari);
+
+})()
+
+function preguntaUsuari() {
+  return inquirer.prompt([
+   {
+     type: 'input',
+     name: 'usuari',
+     message: 'Nom del usuari?',
+   }
+ ])
+   .then((answers) => {
+     usuari = answers.usuari;
+     console.log(`T'has registrat com ${usuari}`);
+   })
+   .catch((error) => {
+     if (error.isTtyError) {
+       // Prompt couldn't be rendered in the current environment
+     } else {
+       // Something else went wrong
+     }
+   });
+
 }
-
-async function preguntaInicial() {
+ 
+function preguntaMenu(usuari) {
    return inquirer.prompt([
     {
       type: 'list',
       name: 'funcio',
-      message: 'Quina funcio vols realitzar?',
+      message: 'Quina funció vols realitzar?',
       choices: ['Llistar tasques', 'Llistar una tasca', 'Crear una tasca', 'Actualitzar una tasca', 'Borrar una tasca',new inquirer.Separator(),'Sortir programa'],
     }
   ])
     .then((answers) => {
-      distribucioPreguntes(answers);
+      distribucioPreguntes(answers,usuari);
     })
     .catch((error) => {
       if (error.isTtyError) {
@@ -33,14 +62,14 @@ async function preguntaInicial() {
       }
     });
  
-
 }
 
-async function distribucioPreguntes(primeraResposta) {
+async function distribucioPreguntes(primeraResposta,usuari) {
 
   switch (primeraResposta.funcio) {
     case 'Crear una tasca':
-      controlLocal.creatasca()
+      // controlLocal.crearTasca("francesc",'prova1','finalitzada','avui','')
+      await controllerInquirer.inquirerCrearTasca(usuari)
       break;
     case 'Actualitzar una tasca':
       controlLocal.actualitzarFitxer(1, 1);
@@ -55,8 +84,10 @@ async function distribucioPreguntes(primeraResposta) {
       controlLocal.llistarTotesLesTasques();
       break;
     case 'Sortir programa':
-      console.log('Has sortit del programa')
+      console.log('Has sortit del programa');
+      process.exit()
       break;
-  }
+  } 
+  console.log('Menú principal')
+  preguntaMenu(usuari);
 }
-
