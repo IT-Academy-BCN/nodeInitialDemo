87e2ec5577d96bdb05e2c45df13284eeaf389003
MK <caprichismo@gmail.com>
2022-10-10 19:16:53 +0200

    cleaning up index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,31 @@
 require('dotenv').config();
 
-const express = require('express');
-
+//preparing server and socket server
+const express = require('express')
 const app = express();
-
-//create server
-const server = require('http').Server(app);
-
-//create socket server
+const server = require('http').Server(app)
 const io = require('socket.io')(server);
 
-//connect to DB
-require('./utils/connectDB.js')();
+// Connect to database
+require('./utils/connectDB')();
 
-//middleware
+// Middlewares
 app.use(express.json());
-//express static middleware serving the front
-//if run on separate ports cors must be required
-app.use(express.static("../public/"));
 
+//express static middleware serving the front end
+//if run on separate ports, cors is required
+app.use(express.static("../public/"));
 
 // Routes
 app.use('/register', require('./routes/register'));
 app.use('/login', require('./routes/login'));
 app.use('/auth', require('./routes/auth'));
 
-//invalid route handling
+//handling error route
 app.use((req, res) => res.status(404).send({ status: "fail", message: "PAGE NOT FOUND"}));
 
-//requiering and executing sockets
+// Sockets
 require('./sockets/sockets')(io);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Socket.IO server running at ${port}`);
-  });
+PORT = process.env.API_PORT || 3000
+server.listen(PORT, console.log(`Server listening on ${PORT}`));
