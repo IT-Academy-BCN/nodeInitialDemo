bf4b8d033ffc9865e6b5a29751b3f4d0d3a6a087
MK <caprichismo@gmail.com>
2022-10-13 13:31:12 +0200

    add db name

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -5,7 +5,7 @@ module.exports = async () => {
     try {
     let mongoDB = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
     await mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-    console.log('Connected to DB');
+    console.log(`Connected to ${process.env.DB_NAME} DB`);
     } catch (err) {
         console.log(err.message);
     }
