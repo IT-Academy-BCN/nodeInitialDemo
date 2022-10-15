89b765dd969ab8ebdbb848c5d3b91d1f7928d8c9
MK <caprichismo@gmail.com>
2022-10-15 17:22:51 +0200

    adding missing classes in queries

--- public/js/login.js
+++ public/js/login.js
@@ -1,40 +1,32 @@
-       document.querySelector(".login-form").addEventListener("submit", e => {
-        e.preventDefault();
-    
-        sessionStorage.clear();
-       //assigning variables from input fields
-        const userName = document.querySelector('[name="login-username"]').value;
-        const password = document.querySelector('[name="login-password"]').value;
-        console.log(userName + password);
-        
-        //fetch result from authentication
-        fetch('http://localhost:3000/login', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json' },
-            body: JSON.stringify({userName, password})
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === "success") {
-    
-                //storing session variables 
-                sessionStorage.userId = data.user.userId;
-                sessionStorage.userName = data.user.userName;
-                sessionStorage.accessToken = data.accessToken;
-    
-                // and jump to chat page
-                window.location.replace('../html/chat.html');
-    
-            } else {
-                document.getElementById("login-error").innerHTML = data.message;
-            }
-        }).catch(err => document.getElementById("login-error").innerHTML = err.message);
-    });
+document.querySelector('.entry-form').addEventListener("submit", e => {
+    e.preventDefault();
 
+    sessionStorage.clear();
 
- 
-    
+    const userName = document.querySelector('.entry-form [name="username"]').value;
+    const password = document.querySelector('.entry-form [name="password"]').value;
+    const apiUrl = 'http://localhost:3000';
 
+    fetch(apiUrl +'/login', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({userName, password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
 
+            // Store session variables
+            sessionStorage.userId = data.user.userId;
+            sessionStorage.userName = data.user.userName;
+            sessionStorage.accessToken = data.accessToken;
 
+            // Go to chat window
+            window.location.assign('../html/chat.html');
+
+        } else {
+            document.getElementById("login_error").innerHTML = data.message;
+        }
+    }).catch(err => document.getElementById("login_error").innerHTML = err.message);
+});
 
