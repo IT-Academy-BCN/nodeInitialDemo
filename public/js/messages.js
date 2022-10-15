c3f956a856d4e98708c4061989287aa4afe59add
MK <caprichismo@gmail.com>
2022-10-15 17:38:05 +0200

    proper commenting

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,64 +1,72 @@
-//retrieve message from chat-form input, room and user from sessionStorage
-document.querySelector('.chat-form button').addEventListener('click',() => {
-    const text = document.querySelector('.chat-form input').value;
+const sendMessage = (form) => {
+    //retrieve message from chat-form input, room and user from sessionStorage
+    const text = form.newMessage.value;
     const user = {userId: sessionStorage.userId, userName: sessionStorage.userName};
     const room = {roomId: sessionStorage.roomId, roomName: sessionStorage.roomName};
     
-    //send message with room and user info to backend
+    //send obj with message, room and user info to backend
+    if (text) {
     if (text) {
         let message = {user, room, text};
         socket.emit('new-message', message);
-        outputMessage(message);
-        text = '';
+        displayMessage(message);
+        form.newMessage.value = '';
     }
 
     return false;
-})
+ }
+}
 
 //outout chat messages to dom
-const outputMessage = (message) => {
+const displayMessage = (message) => {
     
     // current message list on display
-    let messageList = document.querySelector("#message-list");
+    let messageList = document.getElementById("messageList");
 
-    // create element to eventually append to current message list
+    // create element with current message to append to current message list
     let li = document.createElement('li');
-    li.classList.add('chat-list')
+    li.classList.add('chat__li')
     li.textContent = message.text;
 
-    // Get the last inserted Ul rom list
-    let ul = document.getElementById('last-message');
-    
-    if(ul)
-    {document.getElementById("last-message").removeAttribute("id");}
+    // retrieve last inserted ul element 
+    let ul = document.getElementById('lastMessage');
+
+    // append if last message has same user.id as current
+    if (ul && (ul.getAttribute('userId') === message.user.userId)) {
+        
+        ul.append(li)
+    } else {
+        if (ul) document.getElementById("lastMessage").removeAttribute("id");
 
         // Create new ul element
         ul = document.createElement('ul');
-        ul.setAttribute('id', 'last-message');
-        ul.setAttribute('user-id', message.user.userId)
+        ul.setAttribute('id', 'lastMessage');
+        ul.setAttribute('userId', message.user.userId)
 
-        // align user's messages to the right
+         // separate user from users 
+         // align user's messages to the right (css)
         if (message.user.userId === sessionStorage.userId) {
-            ul.classList.add('user-message')
+            ul.classList.add('myMessage')
         } else {
-         // align other users's messages to the left
+            // align other users's messages to the left (css)
             const name = document.createElement('li')
             name.textContent = message.user.userName;
-            messageList.appendChild(name);
-            ul.classList.add('alien-message')
+            messageList.append(name);
+            ul.classList.add('notMyMessage')
         }
-        
-        ul.appendChild(li);
-
-        messageList.appendChild(ul);
-   
-      
+        //append list element
+        ul.append(li);
+        //append ul to message list
+        messageList.append(ul);
+    }
+    //scroll to bottom
     messageList.scrollTop = messageList.scrollHeight;
 }
 
 //output user joining room
+const displayJoinMessage = (message) => {
 
-const joinedMessage = () => {
+    // console.log('displayJoinMessage', message)
 
     document.getElementById('lastMessage').removeAttribute('id');
 
@@ -66,12 +74,13 @@ const joinedMessage = () => {
 
     // Create the element to append
     let li = document.createElement('li');
-    li.classList.add('chat-list-join')
+    li.classList.add('chat__li--join')
     li.textContent = message;
     li.setAttribute('id', 'lastMessage');
+    
 
-    messageList.appendChild(li);
+    messageList.append(li);
+
+    //scroll to bottom
     messageList.scrollTop = messageList.scrollHeight;
 }
-
-
