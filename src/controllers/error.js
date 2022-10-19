563997908d75e6f3d97f72219e86afc9a6c85bce
Jess <jessicafala@gmail.com>
2022-10-19 20:09:27 +0200

    suprimido

--- src/controllers/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const error = ((req, res) => {
-  res.status(404).json({message:'404',error: "Página no encontrada. // La pàgina no existeix. // Page not found"})
-})
-
-module.exports = error;
