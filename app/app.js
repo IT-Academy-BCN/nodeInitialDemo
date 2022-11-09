a68d8dc167de391e34a62c61754d6eec9fcfa21b
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-11-09 12:33:46 +0100

    app.js set-up

--- app/app.js
+++ app/app.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.set('view-engine', 'ejs');
+app.use(express.json());
+
+
+//basic info for main page
+app.get('/', (req, res) => {
+    res.send(`Go to /user for json \nGo to /upload for image`)
+})
+
+//user information
+const user = require('./routes/user');
+app.use('/user', user)
+
+//img upload
+const image = require('./routes/imgUpload');
+app.use('/upload', image)
+
+app.listen(port, () =>console.log(`Running on http://localhost:${port}`))
