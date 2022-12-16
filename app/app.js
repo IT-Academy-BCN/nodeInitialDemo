5d090f605f7a3a372cebafb3096c73cd935cce13
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-16 17:36:13 +0100

    setup nivell 2

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ import createError from "http-errors";
 import logger from "morgan";
 import usersRouter from "./routes/user.js";
 import fileUpload from "express-fileupload";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -12,11 +15,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use('/user', usersRouter);
+app.use('/players', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)));
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 
-router.get();
+router.get( '/' );
 
 
 export default router;
