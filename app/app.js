a62e916e6d78037ea8f0050aa2d795b17bc25429
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-16 17:50:23 +0100

    config mysql

--- app/app.js
+++ app/app.js
@@ -4,23 +4,27 @@ import logger from "morgan";
 import usersRouter from "./routes/user.js";
 import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
+import { dbConnectMysql } from "./config/mysql.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(fileUpload());
+app.use( fileUpload() );
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use( logger( 'dev' ) );
+app.use( express.json() );
+app.use( express.urlencoded( { extended: false } ) );
 
-app.use('/players', usersRouter);
+app.use( '/players', usersRouter );
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => next(createError(404)));
+app.use( ( req, res, next ) => next( createError( 404 ) ) );
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen( PORT, () => console.log( `Server is running on http://localhost:${ PORT }` ) );
+
+
+dbConnectMysql();
--- app/config/mysql.js
+++ app/config/mysql.js
@@ -1,3 +1,32 @@
+/* eslint-disable no-undef */
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
+
+const database = process.env.DB_NAME;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+
+const sequelize = new Sequelize( database, username, password, {
+  host,
+  dialect: "mysql",
+  frozenTableName: true,
+} );
+
+
+const dbConnectMysql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log( "Connection has been established successfully." );
+  } catch ( error ) {
+    console.error( "Unable to connect to the database:", error );
+  }
+};
+
+
+export {
+  sequelize,
+  dbConnectMysql
+};
