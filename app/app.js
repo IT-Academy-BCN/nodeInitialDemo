75630fcbf0467d2becd1499b4981c7a6c1a88f7c
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-10 12:32:27 +0100

    dios sabe que intenté lo del enrutador

--- app/app.js
+++ app/app.js
@@ -2,9 +2,12 @@ import express from "express";
 import logger from "morgan";
 import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
-import { error404 } from "./routes/error404.js";
 import conexion from "./config/mongoConfig.js";
-import routers from "./routes/enrutador.js";
+import { error404 } from "./routes/error.404.js";
+import usersRouter from "./routes/user.js";
+import rankingRouter from "./routes/ranking.js";
+import gamesRouter from "./routes/games.js";
+
 
 
 dotenv.config();
@@ -12,16 +15,17 @@ dotenv.config();
 conexion();
 
 const app = express();
-
 app.use( fileUpload() );
+
+app.use( '/players', usersRouter );
+app.use( '/ranking', rankingRouter );
+app.use( '/games', gamesRouter );
 
 app.use( logger( 'dev' ) );
 app.use( express.json() );
 app.use( express.urlencoded( { extended: false } ) );
-
-app.use( routers );
-
 app.use( error404 );
+
 
 
 const PORT = process.env.PORT || 3000;
--- app/routes/error.404.js
+++ app/routes/error.404.js
@@ -1,7 +1,9 @@
-import express from 'express';
+import express from "express";
 
-export const router = express.Router();
+const router = express.Router();
 
 export const error404 = router.get( '*', ( req, res ) => {
   res.status( 404 ).json( { error: '404 Not Found' } );
 } );
+
+
