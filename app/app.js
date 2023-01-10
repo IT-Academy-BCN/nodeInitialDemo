24608bd44c6f0ac3a1e66a4ca2d2c82fb6512d7f
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-10 10:44:58 +0100

    enrutador del diablo

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 import express from "express";
 import logger from "morgan";
-import usersRouter from "./routes/user.js";
-import rankingRouter from "./routes/ranking.js";
-import gamesRouter from "./routes/games.js";
 import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
 import { error404 } from "./routes/error404.js";
-import  conexion  from "./config/mongoConfig.js";
+import conexion from "./config/mongoConfig.js";
+import routers from "./routes/enrutador.js";
+
 
 dotenv.config();
 
@@ -20,9 +19,7 @@ app.use( logger( 'dev' ) );
 app.use( express.json() );
 app.use( express.urlencoded( { extended: false } ) );
 
-app.use( '/players', usersRouter ); // TODO - CREAR ENRUTADOR
-app.use( '/ranking', rankingRouter );
-app.use( '/games', gamesRouter );
+app.use( routers );
 
 app.use( error404 );
 
