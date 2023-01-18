4b1cee3364435577fb77c5634c00dbd016149cb2
Gabriel Valiente <gsvalient@gmail.com>
2023-01-18 21:20:39 +0100

    minor changes

--- app/app.js
+++ app/app.js
@@ -11,7 +11,6 @@ const app = express();
 require('./db/db.connect');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', userRouter);
 app.use('/api/games', gameRouter);
