5e3c69297102b70eb6a88c7e65b0df96e51c7cd0
Gabriel Valiente <gsvalient@gmail.com>
2023-01-18 20:26:49 +0100

    refactor ranking

--- app/controllers/ranking.controllers.js
+++ app/controllers/ranking.controllers.js
@@ -1,31 +1,34 @@
 const getPercentage = require('./win-percent.controller');
 
+async function sortRanking() {
+  const list = await getPercentage();
+  list.sort((a, b) => b.winPercent - a.winPercent);
+  return list;
+}
+
 async function getRanking(req, res) {
   try {
-    const list = await getPercentage();
-    list.sort((a, b) => b.winPercent - a.winPercent);
-    res.status(200).json(list);
+    const sortedList = await sortRanking();
+    res.status(200).json(sortedList);
   } catch (err) {
-    res.status(500).json(err.message);
+    res.status(500).json({ success: false, msg: err.message });
   }
 }
 
 async function getWinner(req, res) {
   try {
-    const list = await getPercentage();
-    list.sort((a, b) => b.winPercent - a.winPercent);
-    const winner = list[0];
+    const sortedList = await sortRanking();
+    const winner = sortedList[0];
     res.status(200).json(winner);
   } catch (err) {
-    res.status(500).json(err.message);
+    res.status(500).json({ success: false, msg: err.message });
   }
 }
 
 async function getLoser(req, res) {
   try {
-    const list = await getPercentage();
-    list.sort((a, b) => b.winPercent - a.winPercent);
-    const loser = list.at(-1);
+    const sortedList = sortRanking();
+    const loser = sortedList.at(-1);
     res.status(200).json(loser);
   } catch (err) {
     res.status(500).json(err.message);
