a4f93476c42084fbee1b88d22327893b86ced647
Gabriel Valiente <gsvalient@gmail.com>
2023-01-18 14:39:26 +0100

    page not found middleware added

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const pageNotFound = require('./middlewares/not-found');
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('HOMEPAGE');
 });
-
+app.use(pageNotFound);
 const PORT = process.env.PORT || 3000;
 const start = async () => {
   // connect to DB
