52e1858e287e2b5d4d0984d5a25370b98a7e00c3
Gabriel Valiente <gsvalient@gmail.com>
2023-01-19 22:00:42 +0100

    not found middleware added

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const userRouter = require('./routes/user.routes');
+const pageNotFound = require('./middlewares/not-found.middleware');
 
 const app = express();
 
 app.use('/user', userRouter);
+
+app.use(pageNotFound);
 
 const PORT = 3000;
 app.listen(PORT, console.log(`listening on port ${PORT}...`));
