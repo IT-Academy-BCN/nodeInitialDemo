12e3857742357646757103756c9cb5648a9a3754
Gabriel Valiente <gsvalient@gmail.com>
2023-01-19 17:24:20 +0100

    started server

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const startServer = async () => {
+  //connect DB
+  app.listen(PORT, console.log(`listening on port ${PORT}...`));
+};
+startServer();
