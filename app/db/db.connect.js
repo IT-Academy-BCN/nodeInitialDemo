6b9993e44ffa3e8526af06057096d983ac0d781b
Gabriel Valiente <gsvalient@gmail.com>
2023-01-19 17:27:13 +0100

    db connection started

---
+++ app/db/db.connect.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
