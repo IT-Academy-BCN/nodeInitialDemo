ef25a7382147fae66877b340e159cda20187a783
Gabriel Valiente <gsvalient@gmail.com>
2023-01-19 22:19:17 +0100

    multer functions created

---
+++ app/controllers/upload.controller.js
@@ -0,0 +1,32 @@
+const multer = require('multer');
+const { join, extname } = require('path');
+
+//* VARIABLES FOR MULTER
+const PATH = join(__dirname, '..', 'uploads');
+const MIMES = ['image/jpeg', 'image/png', 'image/gif'];
+
+//* MULTER FUNCTION
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: PATH,
+    filename: (req, file, cb) => {
+      const extension = extname(file.originalname);
+      const fileName = file.originalname.split(extension)[0];
+
+      cb(null, `${fileName}_${Date.now()}${extension}`);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (MIMES.includes(file.mimetype)) cb(null, true);
+    else cb(new Error(`Only ${MIMES.join(' ')} files are allowed`));
+  },
+  limits: {
+    fieldSize: 1000000,
+  },
+});
+
+const sendFile = (req, res) => {
+  res.status(200).json({ success: true, msg: 'file uploaded' });
+};
+
+module.exports = { upload, sendFile };
--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { upload, sendFile } = require('../controllers/upload.controller');
 
 const router = express.Router();
 
-router.route('/').post();
+router.route('/').post(upload.single('image'), sendFile);
 
 module.exports = router;
