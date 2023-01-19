5590edb6f22209ce96c6474f51acebe055523b25
Gabriel Valiente <gsvalient@gmail.com>
2023-01-19 18:11:52 +0100

    modify user created

--- app/controllers/user.controllers.js
+++ app/controllers/user.controllers.js
@@ -42,7 +42,27 @@ async function getUsers(req, res) {
   }
 }
 
+async function modifyUser(req, res) {
+  try {
+    const { id } = req.params;
+    const newUsername = req.body;
+    const user = await User.findOne({
+      where: { id },
+    });
+    if (!user) {
+      return res.status(404).json({ success: false, msg: 'cannot find user' });
+    }
+    await User.update(newUsername, {
+      where: { id },
+    });
+    res.status(201).json({ success: true, msg: 'user updated' });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err.message });
+  }
+}
+
 module.exports = {
   createUser,
   getUsers,
+  modifyUser,
 };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { createUser, getUsers } = require('../controllers/user.controllers');
+const {
+  createUser,
+  getUsers,
+  modifyUser,
+} = require('../controllers/user.controllers');
 
 const router = express.Router();
 
 router.route('/').get(getUsers).post(createUser);
+router.route('/:id').patch(modifyUser);
 
 module.exports = router;
