617b024e0c33cd7815db9674bff6ab8d88d51395
Gabriel Valiente <gsvalient@gmail.com>
2023-01-20 13:12:25 +0100

    Created the object that is sent back to the user

--- app/controllers/pokemon.controllers.js
+++ app/controllers/pokemon.controllers.js
@@ -1,9 +1,17 @@
 async function getPokemon(req, res) {
-  const { id } = req.params;
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const result = await data.json();
-  console.log(result.name, result.height, result.weight);
-  res.send('test');
+  try {
+    const { id } = req.params;
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemon = await data.json();
+    res.status(200).json({
+      success: true,
+      name: pokemon.name,
+      height: pokemon.height,
+      weight: pokemon.weight,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
 }
 
 module.exports = getPokemon;
