0e4525a547e6699e6017317e18414b30e3545669
Gabriel Valiente <gsvalient@gmail.com>
2023-01-20 12:58:52 +0100

    started server with express

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('TESTING SERVER'));
+
+const PORT = 3000;
+app.listen(PORT, console.log(`listening on port ${PORT}...`));
