a8a623949249b12257b624b5dc3c30a4c093269b
Gabriel Valiente <gsvalient@gmail.com>
2023-01-20 11:08:14 +0100

    Added cors to dep and to server

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const timeRouter = require('./routes/time.routes');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/time', timeRouter);
 
