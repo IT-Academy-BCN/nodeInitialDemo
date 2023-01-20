ad39d5a0913f80a47cba70da3f849932e2ec55b1
Gabriel Valiente <gsvalient@gmail.com>
2023-01-20 10:30:11 +0100

    Deleted unused directories and initialized app with express

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('TESTING'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`listening on port ${PORT}`));
