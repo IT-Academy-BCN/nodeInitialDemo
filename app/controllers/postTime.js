079c940390b3591699e23701d25beb6ad43b3c8d
David Iglesias <im.david85@gmail.com>
2023-03-27 19:06:49 +0200

    correcciones

--- app/controllers/postTime.js
+++ app/controllers/postTime.js
@@ -1,7 +1,3 @@
-const express = require("express")
-const fs = require("fs")
-const users = require("../users/user.json")
-
 const postTime = (req, res) => {
   const day = new Date()
   res.send(day)
